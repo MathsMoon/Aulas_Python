976fb6fa9444f1bab1eb4e30243b1e13ca87ab0f
Matheus A <matheusalvessouza1@hotmail.com>
2024-03-19 20:38:03 -0300

    Update 19_03
    Mais uma Aula de python, sobre condições. Vou começar a usar o VScode para estudar essa linguagem.

---
+++ Python_VsCode/Condicoes.py
@@ -0,0 +1,20 @@
+#Aula sobre condições no Python -- Apresentando média de aluno
+
+#Criação de variáveis e inserção dos dados
+Aluno = input('Escreva seu nome: ') #Opcional
+repetente = False
+nota1 = float (input('Escreva sua primeira nota: '))
+nota2 = float (input('Escreva sua segunda nota: '))
+nota3 = float (input('Escreva sua terceira nota: '))
+nota4 = float (input('Escreva sua quarta nota: '))
+
+#Cálculo da Média
+resultado = (nota1 + nota2 + nota3 + nota4)/4
+
+#Condições para o {Aluno} passar, reprovar ou ir de recuperação.
+if (resultado >= 7):
+    print(f'{Aluno} sua média foi de: {resultado}, você passou de ano')
+elif (resultado < 7 and repetente == False):
+    print(f'{Aluno} sua média foi de: {resultado}, você foi para a recuperação')
+else:
+    print(f'{Aluno} sua média foi de: {resultado}. você foi reprovado')
